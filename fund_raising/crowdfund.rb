fe3f61a280cf1149ceb98c926aceedf4164ff1bd
Steve Roma <sjroma@hotmail.com>
2017-05-04 17:17:47 -0400

    lesson3: updated studio_game.rb, created new fundraising repository

---
+++ fund_raising/crowdfund.rb
@@ -0,0 +1,9 @@
+project1 = "Project ABC"
+project2 = "Project LMN"
+project3 = "Project XYZ"
+
+fund1 = 1000
+
+puts "Project #{project1} has $#{fund1} in funding."
+puts
+puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,8 +1,19 @@
-greeting = "Welcome!"
+player1 = "larry"
+health1 = 60
+# use a single quoted string to print the player and his health
+puts player1 + '\'s health is ' + health1.to_s
+# use a double quoted string to print the player and his health
+puts "#{player1}'s health is #{health1}"
+# change the double quoted string to triple the players health
+puts "#{player1}'s health is #{health1 * 3}"
+# divide larry's health by 9.  Print answer with decimals (float) and without (integer)
+puts "#{player1}'s health is #{health1 / 9.0}"
+puts "#{player1}'s health is #{health1 / 9}"
 
-3.times do
-  puts greeting.upcase
-end
+puts
+puts "Players: \n\tlarry\n\tcurly\n\tmoe"
 
-puts Time.now
-puts Time.new
+player1 = "larry"
+player2 = "curly"
+player3 = "moe"
+puts "Players: \n\t#{player1}\n\t#{player2}\n\t#{player3}"
